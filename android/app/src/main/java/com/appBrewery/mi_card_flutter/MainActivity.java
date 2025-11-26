36a968a6e3ab56340b1218bb0b508fb94439e3ec
NonymousMorlock <agamspaul@gmail.com>
2025-11-26 09:16:11 +0000

    🗑 chore(migration): Migrate to Flutter 3.22, Gradle 9.2.1, and update Android/iOS configs

--- android/app/src/main/java/com/appBrewery/mi_card_flutter/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.appBrewery.mi_card_flutter;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/appBrewery/mi_card_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.appBrewery.mi_card_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
